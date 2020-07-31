88769e302befdc65cbf0601987922118da0762c2
vova <vovazav172839@gmail.com>
2020-07-31 13:01:30 +0300

    add env variables

--- build_app.sh
+++ build_app.sh
@@ -1,39 +1,33 @@
 #!/usr/bin/env bash
 
-SERVER_IP="192.168.60.3"
-
-DB_IP="192.168.60.5"
-# TODO
-DATABASE="dtapi2"
-DB_USER_NAME="dtapi"
-DB_USER_PSWD="dtapi"
+source ./config.sh
 
 DIST_DIR="app/dtapi"
 DIST_DIR_BE="app/dtapi/api"
-
 cd build
 mkdir -p "$DIST_DIR_BE/"
 
+
 # build Backend
-git clone https://github.com/koseven/koseven.git
-cp -r koseven/modules ${DIST_DIR_BE}
-cp -r koseven/system ${DIST_DIR_BE}
-cp koseven/public/index.php ${DIST_DIR_BE}
+build_backend() {
+  git clone https://github.com/koseven/koseven.git
+  cp -r koseven/modules ${DIST_DIR_BE}
+  cp -r koseven/system ${DIST_DIR_BE}
+  cp koseven/public/index.php ${DIST_DIR_BE}
 
-git clone https://github.com/yurkovskiy/dtapi.git
-cp -r dtapi/application ${DIST_DIR_BE}
-cp dtapi/.htaccess ${DIST_DIR_BE}
-mkdir -p "$DIST_DIR_BE/application/cache/"
-mkdir -p "$DIST_DIR_BE/application/logs/"
+  git clone https://github.com/yurkovskiy/dtapi.git
+  cp -r dtapi/application ${DIST_DIR_BE}
+  cp dtapi/.htaccess ${DIST_DIR_BE}
+  mkdir -p "$DIST_DIR_BE/application/cache/"
+  mkdir -p "$DIST_DIR_BE/application/logs/"
 
-# edit files
-sed -i -e "s/RewriteBase \//RewriteBase \/api/g" "$DIST_DIR_BE/.htaccess"
-sed -i -e "s/'base_url'   => '\/'/'base_url'   => '\/api\/'/g" \
-"$DIST_DIR_BE/application/bootstrap.php"
-sed -i -e "s/'type'       => 'PDO_MySQL'/'type'       => 'PDO'/g" \
-"$DIST_DIR_BE/application/config/database.php"
-sed -i -e "s/localhost/$DB_IP/g" \
-"$DIST_DIR_BE/application/config/database.php"
+  # edit files
+  sed -i -e "s/RewriteBase \//RewriteBase \/api/g" "$DIST_DIR_BE/.htaccess"
+  sed -i -e "s/'base_url'   => '\/'/'base_url'   => '\/api\/'/g" \
+  "$DIST_DIR_BE/application/bootstrap.php"
+  sed -i -e "s/'type'       => 'PDO_MySQL'/'type'       => 'PDO'/g" \
+  "$DIST_DIR_BE/application/config/database.php"
+  sed -i -e "s/localhost/$DB_IP/g" "$DIST_DIR_BE/application/config/database.php"
 
 # create .htaccess for root directory
 cat <<EOF > "$DIST_DIR/.htaccess"
@@ -49,30 +43,41 @@ RewriteCond %{DOCUMENT_ROOT}%{REQUEST_URI} -d
 RewriteRule ^.*$ - [L]
 RewriteRule ^ index.html
 EOF
+  echo "Backend is ready!"
+}
 
-# build Frontend
-# install node version manager
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+build_frontend() {
+  # install node version manager
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 
-# https://unix.stackexchange.com/questions/184508/nvm-command-not-available-in-bash-script
-. ~/.nvm/nvm.sh
+  # https://unix.stackexchange.com/questions/184508/nvm-command-not-available-in-bash-script
+  . ~/.nvm/nvm.sh
 
-nvm install 12.18.2
+  nvm install 12.18.2
 
-# installl angular globally
-npm install -g @angular/cli@8.3.21
+  # install Angular CLI globally
+  npm install -g @angular/cli@8.3.21
 
-# clone git repository and build front-end
-git clone https://github.com/yurkovskiy/IF-105.UI.dtapi.if.ua.io.git
-cd IF-105.UI.dtapi.if.ua.io
-npm install
+  # clone git repository and build front-end
+  git clone https://github.com/yurkovskiy/IF-105.UI.dtapi.if.ua.io.git
+  cd IF-105.UI.dtapi.if.ua.io
+  npm install
 
-# replace address in Angular env variable with local IP
-ENV_REGEXP="s/https:\/\/dtapi\.if\.ua\/api/http:\/\/$SERVER_IP\/api/g"
-sed -i -e ${ENV_REGEXP} src/environments/environment.ts
-sed -i -e ${ENV_REGEXP} src/environments/environment.prod.ts
+  # replace address in Angular env variable with local IP
+  local env_regexp="s/https:\/\/dtapi\.if\.ua\/api/http:\/\/$SERVER_IP\/api/g"
+  sed -i -e ${env_regexp} src/environments/environment.ts
+  sed -i -e ${env_regexp} src/environments/environment.prod.ts
 
-ng build --prod
-cp -a ./dist/IF105/. ../${DIST_DIR}
+  ng build --prod
+  cp -a ./dist/IF105/. ../${DIST_DIR}
+  cd -
+  echo "Frontend is ready!"
+}
 
-echo "Now you can run vagrant up and check $SERVER_IP in your browser..."
+main() {
+  build_backend
+  build_frontend
+  echo "Now you can run \"vagrant up\" and check $SERVER_IP in your browser..."
+}
+
+main "$@"
