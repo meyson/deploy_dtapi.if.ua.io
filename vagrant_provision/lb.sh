0786458c7a26f81e30fdca66dd78cb7a218f83d9
Volodymyr Zavalskyi <vovazav172839@gmail.com>
2020-08-06 14:18:09 +0300

    del ip_hash, add custom logs

--- vagrant_provision/lb.sh
+++ vagrant_provision/lb.sh
@@ -1,17 +1,19 @@
 configure_lb() {
 cat <<EOF > /etc/nginx/conf.d/lb.conf
 upstream lb {
-    ip_hash;
     server $SERVER_IP_1:80;
     server $SERVER_IP_2:80;
 }
+# custom logs
+log_format upstreamlog '[\$time_local] \$remote_addr - \$remote_user - \$server_name \$host to: \$upstream_addr: \$request \$status upstream_response_time \$upstream_response_time msec \$msec request_time \$request_time';
 
 server {
     listen $LB_IP:80;
-
+    gzip on;
     location / {
         proxy_pass "http://lb";
     }
+    access_log /var/log/nginx/access.log upstreamlog;
 }
 EOF
 systemctl reload nginx
